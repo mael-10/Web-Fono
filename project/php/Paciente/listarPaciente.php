ac1a87fa3d9cdf076ad93a2119f154e6f73a30db
Rafael do Carmo Pereira <2005rafaeldocarmo2005@gmail.com>
2024-05-27 16:50:52 -0300

    oi

--- project/php/Paciente/listarPaciente.php
+++ project/php/Paciente/listarPaciente.php
@@ -5,17 +5,22 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="../../css/listagemPaciente.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+    integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Reddit+Sans:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Reddit+Sans:ital,wght@0,200..900;1,200..900&display=swap"
+    rel="stylesheet" />
 
   <link rel="stylesheet" href="../../../src/output.css" />
   <title>Document</title>
-</head> 
+</head>
 
 <body class="flex h-screen bg-white text-black dark:bg-fundo">
-  <nav class="fixed left-0 top-0 h-full w-[65px] bg-fundo p-4 shadow-custom-shadow duration-300 dark:bg-gray-800  dark:text-white" id="nav-lte">
+  <nav
+    class="fixed left-0 top-0 h-full w-[65px] bg-fundo p-4 shadow-custom-shadow duration-300 dark:bg-gray-800  dark:text-white"
+    id="nav-lte">
     <div class="mb-5 w-full cursor-pointer pl-2.5">
       <i class="fa-solid fa-list text-2xl text-white" id="btn-exp"></i>
     </div>
@@ -81,39 +86,55 @@
     </ul>
   </nav>
   <main class="content flex-1 conteudo">
-  <?php
 
-  include_once("../conexao.php");
-
-  $sql = "SELECT *
-                            FROM paciente";
-
-  $resultado = mysqli_query($conexao, $sql);
-  echo "<h2 class='card-title'>Lista de pacientes</h2>";
+    <?php
+    include_once ("../conexao.php");
 
 
-  if (mysqli_num_rows($resultado) > 0) {
-    while ($row = mysqli_fetch_assoc($resultado)) {
+    $sql = "SELECT *
+    FROM paciente";
 
-      echo "<hr>";
-      echo "<p><span class='negrito'> Nome Completo:</span> " . $row['nome_paciente'] . "</p>";
-      echo "<p><span class='negrito'> CPF:</span> " . $row['cpf'] . "</p>";
-      echo "<p><span class='negrito'> RG:</span> " . $row['RG'] . "</p>";
-      echo "<p><span class='negrito'> Email:</span> " . $row['email'] . "</p>";
-      echo "<p><span class='negrito'> Data de nascimento:</span> " . $row['nascimento'] . "</p>";
-      echo "<p><span class='negrito'> Telefone:</span> " . $row['telefone'] . "</p>";
-      echo "<p><span class='negrito'> Endereço:</span> " . $row['endereco'] . "</p>";
-      echo "<p><span class='negrito'> Bairro:</span> " . $row['bairro'] . "</p>";
-      echo "<p><span class='negrito'> Cidade:</span> " . $row['cidade'] . "</p>";
-      echo "<p><span class='negrito'>Cep:</span> " . $row['cep'] . "</p>";
+    
+
+    $resultado = mysqli_query($conexao, $sql);
+    echo "<h2 class='card-title'>Lista de pacientes</h2>";
+
+    if (mysqli_num_rows($resultado) > 0) {
+      echo "<table border='1'>";
+      echo "<tr>
+            <th>Nome Completo</th>
+            <th>CPF</th>
+            <th>RG</th>
+            <th>Email</th>
+            <th>Data de nascimento</th>
+            <th>Telefone</th>
+            <th>Endereço</th>
+            <th>Bairro</th>
+            <th>Cidade</th>
+            <th>Cep</th>
+          </tr>";
+      while ($row = mysqli_fetch_assoc($resultado)) {
+        echo "<tr>";
+        echo "<td>" . $row['nome_paciente'] . "</td>";
+        echo "<td>" . $row['cpf'] . "</td>";
+        echo "<td>" . $row['RG'] . "</td>";
+        echo "<td>" . $row['email'] . "</td>";
+        echo "<td>" . $row['nascimento'] . "</td>";
+        echo "<td>" . $row['telefone'] . "</td>";
+        echo "<td>" . $row['endereco'] . "</td>";
+        echo "<td>" . $row['bairro'] . "</td>";
+        echo "<td>" . $row['cidade'] . "</td>";
+        echo "<td>" . $row['cep'] . "</td>";
+        echo "</tr>";
+      }
+      echo "</table>";
+    } else {
+      echo "Não há registros na tabela.";
     }
-  } else {
-    echo "Não há registros na tabela.";
-  }
 
-  // Fecha a conexão
-  mysqli_close($conexao);
-  ?>
+    // Fecha a conexão
+    mysqli_close($conexao);
+    ?>
   </main>
   <script src="../../javascript/menu.js"></script>
 </body>
